92f86e4ccfc07a6a11bd17e2306fab5a1fe490b3
Данила Сысоев <crocodil-msu@ya.ru>
2026-02-24 09:17:36 +0300

    Create Game as Singleton. Add Width and Height settings

--- RpgRoguelike/Game.cs
+++ RpgRoguelike/Game.cs
@@ -2,7 +2,24 @@ namespace RpgRoguelike;
 
 public class Game
 {
-    public void Init()
+    public int MapWidth { get; private set; } = 100;
+    public int MapHeigth { get; private set; } = 100;
+
+
+    public static Game Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new Game();
+                instance.Init();
+            }
+            return instance;
+        }
+    }
+
+    private void Init()
     {
         gameStopped = false;
         Console.Clear();
@@ -44,4 +61,5 @@ public class Game
     
 
     private bool gameStopped = false;
+    private static Game? instance;
 }
