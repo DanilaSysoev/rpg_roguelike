abc6109dd9cd8bb946179459b8037ec04c5c4e4e
Данила Сысоев <crocodil-msu@ya.ru>
2026-03-03 10:44:22 +0300

    Finalize game

--- RpgRoguelike/Game.cs
+++ RpgRoguelike/Game.cs
@@ -1,3 +1,5 @@
+using RpgRoguelike.Core;
+
 namespace RpgRoguelike;
 
 public class Game
@@ -23,16 +25,15 @@ public class Game
     {
         gameStopped = false;
         Console.Clear();
-        Console.WriteLine("Game is running... Press Escape to stop.");
     }
 
     public void Run()
     {
         while (!GemeIsEnded())
         {
+            Render();
             HandleInput();
             Update();
-            Render();
         }
     }
 
@@ -44,29 +45,111 @@ public class Game
     private void HandleInput()
     {
         var key = Console.ReadKey(true);
-        if (key.Key == ConsoleKey.Escape)
-            gameStopped = true;
+        switch (key.Key)
+        {
+            case ConsoleKey.Escape:
+                gameStopped = true;
+                break;
+            case ConsoleKey.UpArrow:
+                if (room.CanMoveTo(player.Position.UpNeighbor()))
+                    player.Move(Direction.Up);
+                break;
+            case ConsoleKey.RightArrow:
+                if(room.CanMoveTo(player.Position.RightNeighbor()))
+                    player.Move(Direction.Right);
+                break;
+            case ConsoleKey.DownArrow:
+                if (room.CanMoveTo(player.Position.DownNeighbor()))
+                    player.Move(Direction.Down);
+                break;
+            case ConsoleKey.LeftArrow:
+                if (room.CanMoveTo(player.Position.LeftNeighbor()))
+                    player.Move(Direction.Left);
+                break;
+        }
     }
 
     private void Update()
     {
-        // Now empty because we don't have any game logic yet.
+        Cell curr = room.GetCell(player.Position);
+        foreach(var reward in curr.Rewards)
+            reward.Get(player);
+
+        curr.RemoveAllRewards();
     }
 
-    private static void Render()
+    private void Render()
     {
         Console.Clear();
-        Console.WriteLine("Game is running... Press Escape to end game.");
+        ClearDrawBuffer();
+        DrawRoom();
+        DrawPlayer();
+        Flush();
+        DrawStatus();
+    }
+
+    private void DrawStatus()
+    {
+        Console.WriteLine($"Health: {player.Health} / {player.MaxHealth}");
+        Console.WriteLine($"Gold: {player.Gold}");
     }
     
     private Game()
     {
-        MapHeigth = 100;
-        MapWidth = 100;
+        MapHeigth = 15;
+        MapWidth = 40;
+
+        drawBuffer = new char[MapHeigth, MapWidth];
+        room = new Room(MapWidth, MapHeigth);
+        player = new Player(100, 50, new Position(5, 5));
 
         gameStopped = false;
     }
 
+    private void ClearDrawBuffer()
+    {
+        for(int line = 0; line < MapHeigth; ++line)
+        {
+            for(int column = 0; column < MapWidth; ++column)
+            {
+                drawBuffer[line, column] = ' ';
+            }
+        }
+    }
+
+    private void DrawRoom()
+    {
+        foreach(var cell in room.Cells)
+        {
+            if (!cell.IsPassable)
+                drawBuffer[cell.Position.Line, cell.Position.Column] = '#';
+            else if(cell.Rewards.Any())
+                drawBuffer[cell.Position.Line, cell.Position.Column] = '*';
+        }
+    }
+
+    private void DrawPlayer()
+    {
+        drawBuffer[player.Position.Line, player.Position.Column] = '@';
+    }    
+
+    private void Flush()
+    {
+        for(int line = 0; line < MapHeigth; ++line)
+        {
+            for(int column = 0; column < MapWidth; ++column)
+            {
+                if(line < Console.BufferHeight && column < Console.BufferWidth - 1)
+                    Console.Write(drawBuffer[line, column]);
+            }
+            Console.WriteLine();
+        }
+    }
+
     private bool gameStopped;
     private static Game? instance;
+
+    private readonly char[,] drawBuffer;
+    private readonly Room room;
+    private readonly Player player;
 }
